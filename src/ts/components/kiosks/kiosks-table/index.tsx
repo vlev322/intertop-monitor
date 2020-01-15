363ee1d8352d418f55384afaf22e9c67aa21f8a4
vlev322 <vlev322@gmail.com>
2020-01-15 15:18:40 +0200

    refactor(comp/kiosks/kiosk-table) class change on functional component

--- src/ts/components/kiosks/kiosks-table/index.tsx
+++ src/ts/components/kiosks/kiosks-table/index.tsx
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useTitle } from "hookrouter";
 
 import { KiosksService } from "../../../services/getAsyncKiosks";
 
 import { IProps as IKioskListItem } from "../kiosk-list-item/index";
 import { KioskListItem } from "../kiosk-list-item";
 
-interface IState {
-	kiosks: IKioskListItem[];
-}
-
-export class KiosksTable extends Component<{}, IState> {
-	public state = { kiosks: [] };
-	componentDidMount() {
-		const _kiosksService = new KiosksService();
+export const KiosksTable = () => {
+	useTitle("All kiosks");
+	const _kiosksService = new KiosksService();
+	const initialState: IKioskListItem[] = [{ id: 0, name: "", ip: "", city: "" }];
+	const [kiosks, setKiosks] = useState(initialState);
+	useEffect(() => {
 		_kiosksService.getKiosksAsync().then(result => {
-			this.setState({ kiosks: result });
+			setKiosks(result);
 		});
-	}
+	}, []);
 
-	render() {
-		return (
-			<div>
-				{this.state.kiosks.map((kiosk: IKioskListItem) => (
-					<KioskListItem key={kiosk.ip} {...kiosk} />
-				))}
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			{kiosks.map((kiosk: IKioskListItem) => (
+				<KioskListItem key={kiosk.ip} {...kiosk} />
+			))}
+		</div>
+	);
+};
